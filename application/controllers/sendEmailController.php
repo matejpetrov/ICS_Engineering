26abe784d6e5a27ebef50bfa92fa6c536b075082
borka-s <boris.siljan@gmail.com>
2014-12-29 04:53:27 +0100

    Mail Sender
    Isprakjanje na mail raboti ama momentalno ispraka na edna adresa so ja
    imam za test

---
+++ application/controllers/sendEmailController.php
@@ -0,0 +1,40 @@
+<?php
+class SendEmailController extends CI_Controller
+{
+	function sendEmail(){
+		
+		$nameSurname=$this->input->post('name');
+		$email = $this->input->post('email');
+		$subject = $this->input->post('subject');
+		$message = $this->input->post('msg');
+		// echo "<script type='text/javascript'>alert($nameSurname);</script>";
+
+		require (APPPATH. 'third_party/class.phpmailer.php');
+		$mail = new PHPMailer ();
+		$mail->IsSMTP ();
+		$mail->Mailer = 'smtp';
+		$mail->SMTPAuth = true;
+		$mail->Host = 'mail.ekoplast.com.mk'; // "ssl://smtp.gmail.com" didn't worked
+		$mail->Port = 25;
+		$mail->SMTPSecure = '';
+		
+		$mail->Username = "borka@ekoplast.com.mk";
+		$mail->Password = "Kondura2359";
+		
+		$mail->IsHTML ( true ); // if you are going to send HTML formatted emails
+		$mail->SingleTo = true; // if you want to send a same email to multiple users. multiple emails will be sent one-by-one.
+		
+		$mail->From = $email;
+		$mail->FromName = $nameSurname;
+		
+		$mail->addAddress ( "borka@ekoplast.com.mk", "User 1" );
+		
+		$mail->Subject = $subject;
+		$mail->Body = $message;
+		
+		$mail->Send ();
+		echo '<h1>Sucess</h1>' ;
+		redirect('testLanguageController/contact');
+	}	
+}
+?>
--- application/views/contact.php
+++ application/views/contact.php
@@ -3,22 +3,22 @@
 	<div class="row">
 		<div class="col-md-9 contact">
 			<h1><?php echo $contact_page_title; ?></h1>
-			<form>
+			<form method="post" action="<?php echo base_url();?>sendEmailController/sendEmail">
 				<div class="form-group">
 					<label><?php echo $contact_name_surname; ?></label>
-					<input type="text" class="form-control" placeholder="<?php echo $contact_name_surname_placeholder; ?>" aria-describedby="basic-addon1">
+					<input  name="name" type="text" class="form-control" placeholder="<?php echo $contact_name_surname_placeholder; ?>" aria-describedby="basic-addon1">
 				</div>
 				<div class="form-group">
 					<label><?php echo $contact_mail; ?></label>
-					<input type="text" class="form-control" placeholder="<?php echo $contact_mail_placeholder ?>" aria-describedby="basic-addon1">
+					<input name="email" type="text" class="form-control" placeholder="<?php echo $contact_mail_placeholder ?>" aria-describedby="basic-addon1">
 				</div>
 				<div class="form-group">
 					<label><?php echo $contact_title; ?></label>
-					<input type="text" class="form-control" placeholder="<?php echo $contact_title_placeholder; ?>" aria-describedby="basic-addon1">
+					<input name="subject" type="text" class="form-control" placeholder="<?php echo $contact_title_placeholder; ?>" aria-describedby="basic-addon1">
 				</div>
 				<div class="form-group">
 					<label><?php echo $contact_message; ?></label>
-					<textarea class="form-control" placeholder="<?php echo $contact_message_placeholder; ?>" ></textarea>
+					<textarea name="msg" class="form-control" placeholder="<?php echo $contact_message_placeholder; ?>" ></textarea>
 					<button type="submit" class="btn btn-default">
 						<?php echo $contact_btn_send; ?>
 					</button>
