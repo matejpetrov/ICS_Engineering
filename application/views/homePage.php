dea1c9052458799e81884c85b1e2d8f187b77440
borka-s <boris.siljan@gmail.com>
2015-01-25 19:19:10 +0100

    Html tagovi homepage slider
    Html tagovi homepage slider
    Routes za news

--- application/views/homePage.php
+++ application/views/homePage.php
@@ -3,7 +3,9 @@
 <div class="push">
 	<div class="camera_wrap camera_orange_skin camera_emboss pattern_1" id="camera_wrap_4">
 		<?php foreach ($images->result() as $image) { ?>
-		<div  data-src="<?php echo base_url().$image->image_url; ?>"></div>
+		<div  data-src="<?php echo base_url().$image->image_url; ?>">
+		<div class="moveFromRight" style="position:absolute;font-size:20px; top:5%; left:5%; color:#fff; padding:5px; width:25%">The text of your html element</div>
+		</div>
 		<?php } ?>
 	</div>
 </div>
